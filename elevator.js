d6fc3a629907e088a8576adbe11b5e21d470b0bf
Constanti <constantin.dumba@gmail.com>
2015-06-03 09:26:23 -1000

    done i think

--- elevator.js
+++ elevator.js
@@ -2,5 +2,15 @@
 'use strict';
 
 module.exports = {
+  hello : function(argument) {
+    return 'hello';
+  }
+  goto : function(start,end){
+    if(typeof start === 'object' || typeof end === 'object' || start > 3 || start < 0 || end > 3 || end < 0 || start === end){
+      return 0;
+    } else {
+      return Number(end) - Number(start);
+    }
+  }
 
 };
