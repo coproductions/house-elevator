c430ea53a9b674183dc2f4d117f58cdd96566f0e
Constanti <constantin.dumba@gmail.com>
2015-06-03 13:29:05 -1000

    fixed some syntax errors

--- elevator.js
+++ elevator.js
@@ -4,9 +4,9 @@
 module.exports = {
   hello : function(argument) {
     return 'hello';
-  }
+  },
   goto : function(start,end){
-    if(typeof start === 'object' || typeof end === 'object' || start > 3 || start < 0 || end > 3 || end < 0 || start === end){
+    if(typeof start === 'object' || typeof end === 'object' || Number(start) > 3 || Number(start) < 0 || Number(end) > 3 || Number(end) < 0 || Number(start) === Number(end)  || typeof(start)!=="number" || typeof(end)!=='string'){
       return 0;
     } else {
       return Number(end) - Number(start);
